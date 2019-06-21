b8424bf33368ed3c484b087d3db56edf14d8cc46
ghxops <51935312+ghxops@users.noreply.github.com>
2019-06-21 20:11:22 +0000

    style: fix formatting

--- hello-world.js
+++ hello-world.js
@@ -2,11 +2,12 @@ exports.handler = function(event, context) {
   context.succeed("hello world");
 };
 
-function badFormat   (arg)  {
-	const foo = "bar";
-	const baz = 'bar'
+function badFormat(arg) {
+  const foo = "bar";
+  const baz = "bar";
 
-	return { foo, 
-		baz }
-
+  return {
+    foo,
+    baz
+  };
 }
