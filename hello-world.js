564d0173a280ea8b86f79c4785e73104a728a47f
ghxops <51935312+ghxops@users.noreply.github.com>
2019-06-21 13:10:57 -0700

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -2,12 +2,11 @@ exports.handler = function(event, context) {
   context.succeed("hello world");
 };
 
-function badFormat(arg) {
-  const foo = "bar";
-  const baz = "bar";
+function badFormat   (arg)  {
+	const foo = "bar";
+	const baz = 'bar'
 
-  return {
-    foo,
-    baz
-  };
+	return { foo, 
+		baz }
+
 }
